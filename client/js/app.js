d67eef7db3fdf528a59d3f0367ca8278125d3448
Alex Perkins <acperkins@gmail.com>
2016-03-31 18:56:13 -0400

    New story submission working in mock.

--- client/js/app.js
+++ client/js/app.js
@@ -2,13 +2,32 @@
   'use strict';
     adv = adv || (window.adv = {});
 
+    var token = {};
+
     // Call to server to generate a token
     adv.tokenGen = function tokenGen() {
       $.ajax({
         type: 'POST',
         url: '/login',
         contentType: 'application/json',
-        success: function getNameAndToken(data) {
+        success: function getToken(data) {
+          console.log(data);
+          token = data.token;
+        },
+      });
+    };
+
+    // Send the name of the new story to the server
+    adv.createStoryName = function createStoryName() {
+      $.ajax({
+        type: 'POST',
+        url: '/storyname',
+        contentType: 'application/json',
+        headers: {
+            authorization: token
+        },
+        data: JSON.stringify({name: adv.storyName}),
+        success: function getToken(data) {
           console.log(data);
           // userAndToken.token = data.token;
         },
