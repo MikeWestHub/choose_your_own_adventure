9dce95beffb14a027a6f7c637e4c8abe37c4a010
Mike West <westmd23@gmail.com>
2016-04-03 22:08:21 -0400

    All tests passing for authorization

--- server/app.rb
+++ server/app.rb
@@ -14,9 +14,16 @@ before do
   content_type "application/json"
 end
 
-# get "/backend/steps" do
-#   Step.all.to_json
-# end
+
+helpers do
+  def current_session
+    Adventure::Session.where(token: request.env["HTTP_AUTHORIZATION"]).first
+  end
+
+  def halt_unless_session
+    halt 401, {msg: "go away!"}.to_json unless current_session
+  end
+end
 
 post "/login" do
   token = SecureRandom.hex
@@ -26,6 +33,7 @@ end
 
 #create new story
 post "/story" do
+  halt_unless_session
   payload = JSON.parse(request.body.read)
   story = Adventure::Story.create(payload)
   [200, story.to_json]
@@ -34,6 +42,7 @@ end
 
 #create new step
 post "/step" do
+  halt_unless_session
   payload = JSON.parse(request.body.read)
   step = Adventure::Step.create(payload)
   [200, step.to_json]
@@ -41,6 +50,7 @@ end
 
 #list step
 post "/step/:id" do
+  halt_unless_session
   payload = JSON.parse(request.body.read)
   step = Adventure::Step.find(params["id"])
   step.update(payload)
@@ -49,12 +59,14 @@ end
 
 #list all stories
 get "/stories" do
+  halt_unless_session
   stories = Adventure::Story.all
   [200, stories.to_json]
 end
 
 #list all steps of a given story
 get "/steps/:id" do
+  halt_unless_session
   story = Adventure::Story.find(params["id"])
   steps = Adventure::Step.where(story_id: story.id)
   [200, steps.to_json]
@@ -62,6 +74,7 @@ end
 
 #update step
 patch "/step!/:id" do
+  halt_unless_session
   payload = JSON.parse(request.body.read)
   step = Adventure::Step.find(params["id"])
   step.update(payload)
@@ -70,6 +83,7 @@ end
 
 #update story
 patch "/story!/:id" do
+  halt_unless_session
   payload = JSON.parse(request.body.read)
   story = Adventure::Story.find(params["id"])
   story.update(payload)
@@ -78,12 +92,14 @@ end
 
 #delete story
 delete "/story_del/:id" do
+  halt_unless_session
   story = Adventure::Story.find(params["id"])
   story.destroy!
 end
 
 #delete step
 delete "/step_del/:id" do
+  halt_unless_session
   step = Adventure::Step.find(params["id"])
   step.destroy!
 end
--- server/tests/app_test.rb
+++ server/tests/app_test.rb
@@ -7,13 +7,17 @@ class AppTest < Minitest::Test
     Sinatra::Application
   end
 
-  def test_backend_echo_endpoint_will_return_exact_msg_back
-    skip
-    hash = { "name" => "bob" }
-    response = post("/backend/echo", hash.to_json, { "CONTENT_TYPE" => "application/json" })
-    assert response.ok?
-    payload = JSON.parse(response.body)
-    assert_equal(hash, payload)
+  def auth_header
+    token = SecureRandom.hex
+    session = Adventure::Session.create(token: token)
+    header("AUTHORIZATION", token)
+  end
+
+  def test_authorization_token_is_given_in_header
+    auth_header
+    response = get("/stories")
+    auth = JSON.parse(response.body)
+    assert_equal Array, auth.class
   end
 
   def test_can_authenticate_user_request
@@ -23,6 +27,7 @@ class AppTest < Minitest::Test
   end
 
   def test_can_receive_string_for_story_name
+    auth_header
     hash = { name: "Excalibur"}
     response = post("/story", hash.to_json, { "CONTENT_TYPE" => "application/json"})
     story_name = JSON.parse(response.body)
@@ -30,6 +35,7 @@ class AppTest < Minitest::Test
   end
 
   def test_can_recieve_steps
+    auth_header
     hash = { body: "This is a step" }
     response = post("/step", hash.to_json, { "CONTENT_TYPE" => "applicaiton/json" })
     step = JSON.parse(response.body)
@@ -37,6 +43,7 @@ class AppTest < Minitest::Test
   end
 
   def test_create_step_can_receive_options
+    auth_header
     hash = { body: "Begin!!", opt_a: "Go left", opt_b: "Backflip!" }
     response = post("/step", hash.to_json, { "CONTENT_TYPE" => "applicaiton/json" })
     step = JSON.parse(response.body)
@@ -44,6 +51,7 @@ class AppTest < Minitest::Test
   end
 
   def test_can_post_step_based_on_id
+    auth_header
     payload = { body: "Begin!!", opt_a: "Go left", opt_b: "Backflip!" }
     step = Adventure::Step.create(payload)
     response = post("/step/#{step.id}", step.to_json, { "CONTENT_TYPE" => "applicaiton/json" })
@@ -52,6 +60,7 @@ class AppTest < Minitest::Test
   end
 
   def test_can_get_all_steps_for_story
+    auth_header
     story = Adventure::Story.create(name: "Excalibur")
     Adventure::Step.create({ body: "Begin!!", story_id: story.id, opt_a: "Go left", opt_b: "Backflip!", a_assignment: nil, b_assignment: nil })
     Adventure::Step.create({ body: "Left!!", story_id: story.id, opt_a: "Run", opt_b: "Jump!", a_assignment: nil, b_assignment: nil })
@@ -62,6 +71,7 @@ class AppTest < Minitest::Test
   end
 
   def test_can_update_steps_and_options
+    auth_header
     step = Adventure::Step.create(body: "Begin!!", opt_a: "Go left", opt_b: "Backflip!", a_assignment: nil, b_assignment: nil )
     payload = { body: "Begin!!", opt_a: "Go left", opt_b: "Backflip!", a_assignment: 3, b_assignment: 4 }
     response = patch("/step!/#{step.id}", payload.to_json, { "CONTENT_TYPE" => "applicaiton/json" })
@@ -70,6 +80,7 @@ class AppTest < Minitest::Test
   end
 
   def test_that_a_story_can_be_updated
+    auth_header
     story = Adventure::Story.create(name: "Excalibur")
     new_name = {name: "The Sword in the Stone"}
     response = patch("/story!/#{story.id}", new_name.to_json, { "CONTENT_TYPE" => "applicaiton/json" })
@@ -78,16 +89,19 @@ class AppTest < Minitest::Test
   end
 
   def test_that_a_story_can_be_deleted
+    auth_header
     story = Adventure::Story.create(name: "Excalibur")
     delete "/story_del/#{story.id}"
     assert_equal false, Adventure::Story.exists?(story.id)
   end
 
   def test_that_a_step_can_be_deleted
+    auth_header
     story = Adventure::Story.create(name: "Excalibur")
     step_1 = Adventure::Step.create({ body: "Begin!!", story_id: story.id, opt_a: "Go left", opt_b: "Backflip!", a_assignment: nil, b_assignment: nil })
     step_2 = Adventure::Step.create({ body: "Left!!", story_id: story.id, opt_a: "Run", opt_b: "Jump!", a_assignment: nil, b_assignment: nil })
     delete "/step_del/#{step_1.id}"
     assert_equal false, Adventure::Step.exists?(step_1.id)
   end
+
 end
